1de9033851604aede99c1fd03c3fe257dfc2806b
tong <tong@disktree.net>
2018-08-09 19:02:31 +0200

    Cleanup

--- src/App.hx
+++ src/App.hx
@@ -6,39 +6,30 @@ import js.Browser.window;
 import js.html.Element;
 import js.html.InputElement;
 import om.Json;
+import om.FetchTools.*;
 import om.api.youtube.YouTube;
 import om.api.youtube.YouTubePlayer;
-import om.FetchTools.*;
 
 class App {
 
-	static var player : YouTubePlayer;
+	static var isMobileDevice : Bool;
 	static var playlist : Array<String>;
 	static var index : Int;
+	static var player : YouTubePlayer;
+	static var controls : Element;
+	static var loader : Element;
 
-	static function play( ?start : Float ) {
-		player.loadVideoById( playlist[index], start );
-	}
+	static function init( playlistURL = 'playlist.json' ) {
 
-	static function playNext() {
-		if( ++index == playlist.length ) index = 0;
-		play();
-	}
+		index = 0;
 
-	static function init() {
-
-		fetchJson( 'playlist.json' ).then( function(data){
+		fetchJson( playlistURL ).then( function(data){
 
 			playlist = data;
-			index = 0;
 
 			YouTube.init( function(){
 
 				trace( "Youtube ready" );
-
-				var controls = document.getElementById( 'controls' );
-				var loader = document.getElementById( 'loader' );
-				loader.classList.add( 'active' );
 
 				player = new YouTubePlayer( 'youtube-player', {
 					playerVars: {
@@ -54,74 +45,8 @@ class App {
 						loop: 1
 					},
 					events: {
-						'onReady': function(e){
-
-							trace( "Youtube player ready" );
-
-							var volume : InputElement = cast controls.querySelector( 'input[name=volume]' );
-							volume.oninput = e -> {
-								var vol = Std.parseFloat( volume.value );
-								player.setVolume( vol );
-								if( vol == 0 ) {
-									player.pauseVideo();
-								} else {
-									player.playVideo();
-								}
-							}
-
-							var storage = Browser.getLocalStorage();
-							var item = storage.getItem( 'musicforprogramming' );
-							var state = { index: 0, time: 0, volume: 70 };
-							if( item != null ) {
-								state = Json.parse( item );
-								index = state.index;
-								volume.value = Std.string( state.volume );
-								player.setVolume( state.volume );
-							} else {
-								state = { index: 0, time: 0, volume: 70 };
-							}
-
-							play( state.time );
-
-							var overlay = document.getElementById( 'overlay' );
-							overlay.addEventListener( 'click', function(e) {
-								playNext();
-							}, false );
-
-							window.onbeforeunload = function(e){
-								storage.setItem( 'musicforprogramming', Json.stringify( {
-									index: index,
-									time: player.getCurrentTime(),
-									volume: Std.parseFloat( volume.value )
-								} ) );
-								return null;
-							}
-
-						},
-						'onStateChange': function(e){
-							trace(e.data );
-							switch e.data {
-							case unstarted:
-								controls.classList.remove( 'active' );
-								loader.classList.add( 'active' );
-							case buffering:
-							case ended:
-								//controls.style.display = 'none';
-								playNext();
-							case playing:
-								controls.classList.add( 'active' );
-								loader.classList.remove( 'active' );
-								//trace( player.getCurrentTime() );
-							case paused:
-							default:
-								controls.classList.remove( 'active' );
-								loader.classList.add( 'active' );
-							}
-						},
-						'onPlaybackQualityChange': function(e){
-						},
-						'onPlaybackRateChange': function(e){
-						},
+						'onReady': handlePlayerReady,
+						'onStateChange': handlePlayerStateChange,
 						'onError': function(e){
 							trace(e);
 						}
@@ -131,22 +56,100 @@ class App {
 		});
 	}
 
+	static function play( ?start : Float ) {
+		player.loadVideoById( playlist[index], start );
+	}
+
+	static function playNext() {
+		if( ++index == playlist.length ) index = 0;
+		play();
+	}
+
+	static function handlePlayerReady(e) {
+
+		trace( "Youtube player ready" );
+
+		var volume : InputElement = cast controls.querySelector( 'input[name=volume]' );
+		volume.oninput = e -> {
+			var vol = Std.parseFloat( volume.value );
+			player.setVolume( vol );
+			if( vol == 0 ) {
+				player.pauseVideo();
+			} else {
+				player.playVideo();
+			}
+		}
+
+		var storage = Browser.getLocalStorage();
+		var item = storage.getItem( 'musicforprogramming' );
+		var state = { index: 0, time: 0, volume: 70 };
+		if( item != null ) {
+			state = Json.parse( item );
+			index = state.index;
+			volume.value = Std.string( state.volume );
+			player.setVolume( state.volume );
+		} else {
+			state = { index: 0, time: 0, volume: 70 };
+		}
+
+		play( state.time );
+
+		var overlay = document.getElementById( 'overlay' );
+		overlay.addEventListener( 'click', function(e) {
+			playNext();
+		}, false );
+
+		window.onbeforeunload = function(e){
+			storage.setItem( 'musicforprogramming', Json.stringify( {
+				index: index,
+				time: player.getCurrentTime(),
+				volume: Std.parseFloat( volume.value )
+			} ) );
+			return null;
+		}
+	}
+
+	static function handlePlayerStateChange(e) {
+		trace(e.data );
+		switch e.data {
+		case unstarted:
+			controls.classList.remove( 'active' );
+			loader.classList.add( 'active' );
+		case buffering:
+		case ended:
+			//controls.style.display = 'none';
+			playNext();
+		case playing:
+			controls.classList.add( 'active' );
+			loader.classList.remove( 'active' );
+			//trace( player.getCurrentTime() );
+		case paused:
+		default:
+			controls.classList.remove( 'active' );
+			loader.classList.add( 'active' );
+		}
+	}
+
 	static function main() {
 
 		window.onload = function() {
 
-			var isMobile = om.System.isMobile();
+			isMobileDevice = om.System.isMobile();
 
-			if( isMobile ) {
+			controls = document.getElementById( 'controls' );
+			loader = document.getElementById( 'loader' );
+			loader.classList.add( 'active' );
+
+			if( isMobileDevice ) {
+				//TODO mobile devices require initial user interaction
 				document.body.textContent = 'DESKTOP DEVICES ONLY';
 			} else {
 				init();
+				window.oncontextmenu = e -> {
+					e.preventDefault();
+				}
 			}
-
-			window.oncontextmenu = e -> {
-	            e.preventDefault();
-	        }
-		};
+		}
 	}
 
 }
